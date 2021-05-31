3a64b3806f0cbaa17fe2e43bbbca02d76c6aa4a0
Loïc Grobol <loic.grobol@gmail.com>
2021-05-31 10:13:13 +0200

    try to fix cuda failure in DDP by auto-selecting GPUS

--- zeldarose/train_transformer.py
+++ zeldarose/train_transformer.py
@@ -503,6 +503,7 @@ def main(
 
     trainer = pl.Trainer(
         accumulate_grad_batches=accumulate_grad_batches,
+        auto_select_gpus=True,
         callbacks=callbacks,
         default_root_dir=out_dir,
         accelerator=accelerator,
