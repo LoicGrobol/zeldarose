57adedc92b1c4192e806b31dc375bf6b7f601bea
Loïc Grobol <loic.grobol@gmail.com>
2022-01-11 11:33:22 +0100

    use None for gradient clipping default
    It shouldn't change any behaviour but it feels cleaner

--- zeldarose/common.py
+++ zeldarose/common.py
@@ -8,7 +8,7 @@ class TrainConfig(pydantic.BaseModel):
     batch_size: int = 64
     betas: Tuple[float, float] = (0.9, 0.98)
     epsilon: float = 1e-8
-    gradient_clipping = 0
+    gradient_clipping = None
     learning_rate: float = 1e-4
     lr_decay_steps: Optional[int] = None
     warmup_steps: int = 0
